f961da5aeb201a2aaa3050408a55aa3e510d6458
Euller <eullernobrega@gmail.com>
2019-07-31 01:58:01 -0300

    funcionando corretamente

--- src/CadastroNumeros.java
+++ src/CadastroNumeros.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class CadastroNumeros {
     int []vet;
     int pos = 0;
@@ -6,28 +8,34 @@ public class CadastroNumeros {
         this.vet = new int[20];
     }
     public void inserir(int num){
-        if(vet[0] == 0){
-            vet[0] = num;
-            pos++;
-        }else if(consultaPosicao(num) == -1){
-            if(num < vet[0]){ // Caso seja menor que todos os outros numeros
-                for(int i = pos; i > 0; i--){
-                    vet[i] = vet[i-1];
-                 }
+        if(!verificaArrayCheio()){
+            if(vet[0] == 0){
                 vet[0] = num;
-            }else if(num > vet[pos-1]) { // Caso seja maior que todos os outros numeros
-                vet[pos] = num;
-            }else if(localizaPosInsercao(num) != -1) { // Caso seja no meio
-                int x = localizaPosInsercao(num);
-                for(int i = pos; i > x; i--){
-                    vet[i] = vet[i-1];
+                pos++;
+            }else if(consultaPosicao(num) == -1){
+                if(num < vet[0]){ // Caso seja menor que todos os outros numeros
+                    for(int i = pos; i > 0; i--){
+                        vet[i] = vet[i-1];
+                    }
+                    vet[0] = num;
+                }else if(num > vet[pos-1]) { // Caso seja maior que todos os outros numeros
+                    vet[pos] = num;
+                }else if(localizaPosInsercao(num) != -1) { // Caso seja no meio
+                    int x = localizaPosInsercao(num);
+                    for(int i = pos; i > x; i--){
+                        vet[i] = vet[i-1];
+                    }
+                    vet[x] = num;
                 }
-                vet[x] = num;
+                pos++;
+            }else{
+                System.out.println("Numero ja existe");
+                inserir(num+1);
             }
-            pos++;
         }else{
-            System.out.println("Numero ja existe");
+            System.out.println("Array cheio");
         }
+
     }
 
     public int consultaPosicao(int num) {
@@ -45,6 +53,7 @@ public class CadastroNumeros {
         for(int i = 1; i < pos; i++){
             if(num < vet[i]){
                posi = i;
+               break;
             }
         }
         return posi;
@@ -66,6 +75,13 @@ public class CadastroNumeros {
         }else{
             System.out.println("Esse nome nao esta inserido");
         }
+    }
+
+    private boolean verificaArrayCheio(){
+        if(pos == 20){
+            return true;
+        }
+        return false;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,15 +10,15 @@ public class Main {
 
         do{
             System.out.println("Menu\n");
-            System.out.println("1 - Inserir | 2 - Consultar Posicao | 3 - Remover | 4 - Listar | 0 -Sair\n");
+            System.out.println("1 - Inserir valores randomicamente | 2 - Consultar Posicao | 3 - Remover | 4 - Listar | 0 -Sair\n");
             System.out.println("Escolha o que vc deseja: ");
             op = scan.nextInt();
 
             switch (op){
                 case 1:
-                    System.out.println("Informe o numero a ser inserido: ");
-                    num = scan.nextInt();
-                    cadastro.inserir(num);
+                    for(int i = 0; i <= cadastro.pos; i++) {
+                        cadastro.inserir(numRandomico());
+                    }
                     break;
                 case 2:
                     System.out.println("Informe o numero a ser consultado: ");
@@ -36,6 +37,11 @@ public class Main {
             }
         }while(op != 0);
 
+    }
+
+    private static int numRandomico(){
+        Random r = new Random();
+        return r.nextInt(100);
     }
 
 
